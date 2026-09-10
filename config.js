78170bf2a8ce54c01be10e500bfd3f023592f623
Anita <52784073+aticaa@users.noreply.github.com>
2026-09-10 16:54:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Konfiguracja połączenia z Twoim projektem Supabase
+window.TERM_CONFIG = {
+  SUPABASE_URL: "https://oiabhlviecdkstuedmdp.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_IoteuBlBG9exrD5ubm-z4A_9szDC9Md",
+  WORKSPACE_ID: "11111111-1111-1111-1111-111111111111"
+};
